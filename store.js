a4092aa73e5429d4e75e3a3dc2f26274fe17eb5e
Ihekwoaba-Patricia <ihekwoabapatricia@gmail.com>
2024-10-26 15:26:25 -0700

    Add files via upload

---
+++ store.js
@@ -0,0 +1,11 @@
+function bg_color(){
+    document.getElementById("bgcolor").style.backgroundColor="brown"; 
+    document.getElementById("bgcolor").style.fontFamily="Franklin Gothic Medium";
+
+
+    }
+
+     function reduce(){
+         document.getElementById("image").style.opacity="0.36";  
+    }
+ 
